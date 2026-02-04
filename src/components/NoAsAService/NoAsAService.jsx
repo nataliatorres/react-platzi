460d79bb6a41473908a657af42c2e2fa2e4843ea
Natalia Torres Rojas <nataliatorresrojas@gmail.com>
2026-02-04 13:05:28 +1000

    Error handling and loading

--- src/components/NoAsAService/NoAsAService.jsx
+++ src/components/NoAsAService/NoAsAService.jsx
@@ -3,33 +3,50 @@ import { useState, useEffect } from "react";
 const NoAsAService = () => {
     //const [reasons, setReason] = useState([]);
     const [reasons, setReason] = useState([]);
-    const [locked, setUnlocked] = useState(false);
-
-    const handleClick = () => {
-        
-    };
+    const [isLoading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     const fetchData = async () => {
-        fetch("https://naas.isalman.dev/no")
-        //fetch("https://jsonplaceholder.typicode.com/users")
-        .then((response) => response.json())
-        .then((data) => setReason(data))
-        .catch((error) => console.error("Error fetching data: ", error))
+        try {
+            const response = await fetch("https://naas.isalman.dev/no")
+            //fetch("https://jsonplaceholder.typicode.com/users")
+            if (!response.ok) {
+                throw new Error("Error fetching the data")
+            }
+            const data = await response.json()
+            setReason(data)
+            // We don't need these lines anymore
+            // .then((response) => response.json())
+            // .then((data) => {setReason(data);
+            //                 setLoading(false);})
+            // .catch((error) => {console.error("Error fetching data: ", error);})
+        } catch (error) {
+            setError(error.message);
+        } finally {
+            setLoading(false);
+        }
     };
 
     useEffect(() => {
         fetchData();
     }, [])
 
+    if (isLoading)  {
+        return <h1>Loading...</h1>;
+    }
+    if (error) {
+        return <p>There's been an error: {error}</p>;
+    }
+
     return (
-        <div className="w-[100%] border-solid border-red-300 border-1 rounded-xl my-[10px] py-[10px]">
+        <div className="w-[100%] border-solid border-white-300 border-1 rounded-xl my-[50px] py-[10px]">
             <h1>Want to say no but don't know how?</h1>
             <h2>Keep clicking until satisfied with an answer 🫣</h2>
             <button onClick={fetchData}>Give me a reason</button>
-            {//locked && (
-                <p>{reasons.reason}</p>
-            //)
+            {
+                <p className="font-bold">----&gt; {reasons.reason} &lt;----</p>
             }
+            <p className="text-sm">I'm using <a href="https://github.com/hotheadhacker/no-as-a-service">no-as-a-service</a></p>
         </div>
     )
 }
